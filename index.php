675558cadd4c157e2032f984e60868c8399a4ca7
khanh48 <67616753+khanh48@users.noreply.github.com>
2021-10-09 10:14:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,43 @@
-<?php include_once("index.html");
-?>
+<!DOCTYPE html>
+<html lang="vi">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./lib/css/mycss.css">
+    <link rel="shortcut icon" type="image/png" href="./lib/img/logo.png" />
+    <script src="./lib/js/main.js"></script>
+    <title>Happy Birthday</title>
+</head>
+
+<body>
+    <div class="body">
+        <div class="b1">
+        </div>
+        <div class="content">
+            <div class="text">
+                <div class="fonth">Chúc mừng sinh nhật em</div>
+                <div class="fontc">Chúc em có một sinh nhật vui vẻ ý nghĩa, nhiều điều may mắn đến với em, càng ngày
+                    càng xinh đáng yêu hơn ^^.
+                    Anh chưa chuẩn bi được cho em những đồ em thích, không bên em ngày này cũng không đưa em đi ăn
+                    được.
+                    Nên anh chỉ biết làm cái này do anh làm gấp nên không có được đẹp mong em không chê ^^. Chúc em sinh
+                    nhật vui vẻ, hạnh phúc &lt;3
+                </div>
+            </div>
+        </div>
+        <div class="nut">
+            <div class="nd">Bé có yêu anh honggg :></div>
+            <div class="but">
+                <button class="yes">Yêu</button>
+                <button class="no">Không</button>
+                <button class="ib"
+                    onclick="window.location.href='https://www.facebook.com/messages/t/100011243867799';">Dạ
+                    :></button>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
